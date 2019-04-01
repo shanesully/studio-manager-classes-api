51035dda6c203002c8924e3d1d93d5a848dfbd8d
sully <shanesully01@gmail.com>
2019-04-01 19:28:59 +0100

    Spring Initializr content

---
+++ src/main/java/com/studiomanager/studioapi/StudioApiApplication.java
@@ -0,0 +1,13 @@
+package com.studiomanager.studioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioApiApplication.class, args);
+	}
+
+}
